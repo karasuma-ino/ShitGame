4d744ca55f6fba026516d703cb20ef3e7d2d240b
karasuma-ino <03nj.natasa@gmail.com>
2024-08-18 00:10:25 +0900

    ver.0.0.9 たたかうの選択肢の仮設置

--- src/main/java/org/ayato/scene/Battle.java
+++ src/main/java/org/ayato/scene/Battle.java
@@ -1,7 +1,9 @@
 package org.ayato.scene;
 
+import org.ayato.animation.PropertiesComponent;
 import org.ayato.system.ToonMaster;
 import org.ayato.util.BaseScene;
+import org.ayato.util.MaterialUtils;
 
 import java.awt.*;
 
@@ -19,6 +21,11 @@ public class Battle extends BaseScene {
 
     @Override
     public void createUI(ToonMaster toonMaster) {
+        toonMaster.addAnimation("たたかう",
+                                 PropertiesComponent.ofText(100,100,50,30)
+                                                    .font("",0,100)
+                                                    .button(0,0,50,30, MaterialUtils.NORMAL.get(),a->{}
+                                                    ));
 
     }
 }
