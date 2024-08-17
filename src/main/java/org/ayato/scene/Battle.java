b0fb1ce7e171d20c7106f87b33c88e0200685204
karasuma-ino <03nj.natasa@gmail.com>
2024-08-18 01:24:08 +0900

    ver.0.0.9 選択肢の追加

--- src/main/java/org/ayato/scene/Battle.java
+++ src/main/java/org/ayato/scene/Battle.java
@@ -21,11 +21,37 @@ public class Battle extends BaseScene {
 
     @Override
     public void createUI(ToonMaster toonMaster) {
-        toonMaster.addAnimation("たたかう",
-                                 PropertiesComponent.ofText(100,100,50,30)
-                                                    .font("",0,100)
-                                                    .button(0,0,50,30, MaterialUtils.NORMAL.get(),a->{}
-                                                    ));
-
+        toonMaster.addAnimation(  "Enemy spotted!!",
+                                PropertiesComponent.ofText(130,100,50,30)
+                                                   .font("",0,4)
+                               );
+        toonMaster.addAnimation("Week",
+                                PropertiesComponent.ofText(30,150,50,30)
+                                                   .font("",0,3)
+                                                   .button(0,0,50,30, MaterialUtils.NORMAL.get()
+                                                           ,a->{}
+                                                   )
+                               );
+        toonMaster.addAnimation("Heavy",
+                                PropertiesComponent.ofText(80,150,50,30)
+                                                   .font("",0,3)
+                                                   .button(0,0,50,30, MaterialUtils.NORMAL.get()
+                                                           ,a->{}
+                                                          )
+                                );
+        toonMaster.addAnimation("Counter",
+                                PropertiesComponent.ofText(130,150,50,30)
+                                                   .font("",0,3)
+                                                   .button(0,0,50,30, MaterialUtils.NORMAL.get()
+                                                           ,a->{}
+                                                          )
+                               );
+        toonMaster.addAnimation("Gauge",
+                                PropertiesComponent.ofText(180,150,50,30)
+                                                   .font("",0,3)
+                                                   .button(0,0,50,30, MaterialUtils.NORMAL.get()
+                                                           ,a->{}
+                                                          )
+                               );
     }
 }
